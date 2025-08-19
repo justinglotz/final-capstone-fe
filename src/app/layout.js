b0b18e63ce584cbbe71d2e10cdea35a4fc5cb4ea
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-18 20:30:59 -0500

    chore: commented out bootstrap for now

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google';
 import PropTypes from 'prop-types';
 import ClientProvider from '@/utils/context/ClientProvider';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
