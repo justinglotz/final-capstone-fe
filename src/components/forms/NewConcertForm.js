68f3c3d16092ae2320554eea18e0c0afc42d6237
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-19 20:14:35 -0500

    feat: new concert form working in frontend

--- src/components/forms/NewConcertForm.js
+++ src/components/forms/NewConcertForm.js
@@ -2,31 +2,39 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { format } from 'date-fns';
-import { CalendarIcon } from 'lucide-react';
 import { z } from 'zod';
-import { cn } from '@/lib/utils';
 
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Calendar } from '@/components/ui/calendar';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import CalendarInput from '../inputs/CalendarInput';
+import ArtistSearch from '../inputs/ArtistSearch';
+import VenueSearch from '../inputs/VenueSearch';
+
+const artistSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+const venueSchema = z.object({
+  setlist_fm_id: z.string(),
+  name: z.string(),
+  city: z.string(),
+  state: z.string(),
+});
 
 const formSchema = z.object({
-  artistName: z.string(),
-  venueName: z.string(),
+  artist: artistSchema,
+  venue: venueSchema,
   date: z.date(),
   tourName: z.string(),
 });
 
 export default function NewConcertForm() {
-  // Initialize the form
   const form = useForm({
     resolver: zodResolver(formSchema),
   });
 
-  // Submit handler
   function onSubmit(values) {
     console.log(values);
   }
@@ -34,55 +42,9 @@ export default function NewConcertForm() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <Form {...form}>
-        <FormField
-          control={form.control}
-          name="artistName"
-          render={({ field }) => (
-            <FormItem className="w-[500px]">
-              <FormLabel>Artist Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Search for an artist..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="venueName"
-          render={({ field }) => (
-            <FormItem className="w-[500px]">
-              <FormLabel>Venue Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Search for a venue..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="date"
-          render={({ field }) => (
-            <FormItem className="flex flex-col items-start">
-              <FormLabel>Date</FormLabel>
-              <Popover>
-                <PopoverTrigger>
-                  <FormControl>
-                    <Button variant="outline" className={cn('w-[500px] pl-3 text-left font-normal', !field.value && 'text-muted-foreground')}>
-                      {field.value ? format(field.value, 'PPP') : <span>Pick a date</span>}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="center">
-                  <Calendar mode="single" onSelect={field.onChange} captionLayout="dropdown" fromYear={new Date().getFullYear() - 80} toYear={new Date().getFullYear() + 5} />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <ArtistSearch control={form.control} placeholder="Search for artists..." label="Artist Name" url="https://concert-capsule-api.onrender.com/artists/search" name="artist" />
+        <VenueSearch control={form.control} placeholder="Search for venues..." label="Venue Name" url="https://concert-capsule-api.onrender.com/venues/search" name="venue" />
+        <CalendarInput control={form.control} name="date" label="Event Date" placeholder="Choose a date" width="[500px]" />
         <FormField
           control={form.control}
           name="tourName"
---
+++ src/components/inputs/CalendarInput.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { CalendarIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import PropTypes from 'prop-types';
+
+export default function CalendarInput({ control, name, label = 'Date', placeholder = 'Pick a date', width }) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col items-start">
+          <FormLabel>{label}</FormLabel>
+          <Popover>
+            <PopoverTrigger>
+              <FormControl>
+                <Button variant="outline" className={cn(`w-full pl-3 text-left font-normal w-${width}`, !field.value && 'text-muted-foreground')}>
+                  {field.value ? format(field.value, 'PPP') : <span>{placeholder}</span>}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="center">
+              <Calendar mode="single" selected={field.value} defaultMonth={field.value} onSelect={field.onChange} captionLayout="dropdown" fromYear={new Date().getFullYear() - 80} toYear={new Date().getFullYear() + 5} />
+            </PopoverContent>
+          </Popover>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+CalendarInput.propTypes = {
+  // eslint-disable-next-line
+  control: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
