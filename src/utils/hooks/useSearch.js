b2465b3d8bdfe21111e5794c43ed0de4479d7b15
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-26 20:22:00 -0500

    chore: added param prop to useSearch hook to allow for search parameters other than 'q'

--- src/utils/hooks/useSearch.js
+++ src/utils/hooks/useSearch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 // eslint-disable-next-line import/prefer-default-export
-export function useSearch(url, initialQuery = '', delay = 300) {
+export function useSearch(url, initialQuery = '', delay = 300, param = 'q') {
   const [query, setQuery] = useState(initialQuery);
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -16,7 +16,7 @@ export function useSearch(url, initialQuery = '', delay = 300) {
       const fetchData = async () => {
         setLoading(true);
         try {
-          const res = await fetch(`${url}?q=${query}`);
+          const res = await fetch(`${url}?${param}=${query}`);
           const data = await res.json();
           setResults(data);
         } catch (error) {
@@ -32,7 +32,7 @@ export function useSearch(url, initialQuery = '', delay = 300) {
 
     // eslint-disable-next-line consistent-return
     return () => clearTimeout(timeoutId);
-  }, [query, url, delay]);
+  }, [query, url, delay, param]);
 
   return { query, setQuery, results, loading };
 }
