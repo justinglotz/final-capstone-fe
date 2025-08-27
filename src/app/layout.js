e2371925fdb89ef961d3dedf87dfd0e459ce4791
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-26 20:16:14 -0500

    chore: changed website title to ConcertCapsule

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter , Inconsolata } from 'next/font/google';
+import { Inter, Inconsolata } from 'next/font/google';
 import PropTypes from 'prop-types';
 import ClientProvider from '@/utils/context/ClientProvider';
 
@@ -27,7 +27,7 @@ export const generateMetadata = async ({ params }) => {
   const { slug } = params; // Example of accessing dynamic route params
 
   return {
-    title: `TEMPLATE - ${slug || 'HOME'}`, // Dynamically set the title using route parameters
+    title: `ConcertCapsule - ${slug || 'HOME'}`, // Dynamically set the title using route parameters
     description: `This is a dynamically generated description for ${slug}.`, // Dynamic description
     // Add other metadata fields as needed, like keywords, open graph tags, etc.
     keywords: [`${slug}`, 'dynamic', 'page'],
