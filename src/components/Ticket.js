c1764fc15f862b20b0f79e324331d7f173e922f8
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-26 20:20:43 -0500

    chore: adjusted styling of ticket component to allow more room for date and time elements

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -24,7 +24,7 @@ export default function Ticket({ artistName, tourName, venue, city, state, date,
   const dateObj = parseISO(combinedDateTime);
   const formatted = format(dateObj, 'EEEE, MMMM d yyyy, h:mm a');
   return (
-    <div className="w-[490px] h-[220px] border border-black flex flex-row rounded-lg overflow-hidden">
+    <div className="w-[490px] h-[210px] border border-black flex flex-row rounded-lg overflow-hidden">
       <div className="flex-[1] bg-ticket-bg-left flex flex-col items-center gap-3 py-3">
         <Button className="w-12 h-12 rounded-md bg-black text-white flex items-center justify-center">1</Button>
         <Button className="w-12 h-12 rounded-md bg-black text-white flex items-center justify-center">2</Button>
@@ -32,25 +32,24 @@ export default function Ticket({ artistName, tourName, venue, city, state, date,
       </div>
       <div className="flex-[0_0_5px] bg-ticket-center-line"></div>
       <div className="flex-[6] relative bg-ticket-background">
-        {/* Watermark background */}
-        <div className="absolute inset-0 text-[11px] text-gray-300 leading-[18px] overflow-hidden opacity-90 select-none">
+        <div className="absolute inset-0 text-[11px] text-gray-300 leading-[18px] overflow-hidden opacity-95 select-none">
           <div className="transform -rotate-12 origin-top-left translate-x-[-50px] translate-y-[-20px] scale-110">{generateWatermarkRows()}</div>
         </div>
 
         {/* Ticket content would go here */}
         <div className="relative z-10 ">
-          <div className="mt-18 font-inconsolata text-black">
+          <div className="mt-15 font-inconsolata text-black">
             <div className="font-semibold ml-16 text-[22px]">{artistName.toUpperCase()}</div>
-            <div className="ml-10 text-[20px]">{tourName}</div>
+            <div className="ml-8 text-[19px]">{tourName}</div>
             <div className="ml-16 text-[16px]">{venue}</div>
             <div className="ml-16 text-[16px]">
               {city}, {state}
             </div>
-            <div className="ml-10 text-[20px]">{formatted}</div>
+            <div className="ml-8 text-[19px]">{formatted}</div>
           </div>
         </div>
       </div>
-      <div className="flex-[1] text-sm flex items-center justify-center bg-foreground">
+      <div className="flex-[1/2] text-sm flex items-center justify-center bg-foreground">
         <div className="transform -rotate-90 whitespace-nowrap font-bold text-black font-inconsolata text-[16px]">
           <div>TICKET</div>
           <div>#002</div>
