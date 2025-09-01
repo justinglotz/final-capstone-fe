5d951c8581daff9f07ae531521507b1b445ec002
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-09-01 12:01:30 -0500

    feat: social feed component, fixed formatting

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,14 @@
 import React, { useEffect, useState } from 'react';
 import { Separator } from '../components/ui/separator';
 import getNewsFeed from '../api/newsFeedData';
-import Ticket from '../components/Ticket';
+import FeedItem from '../components/feedItem';
 
 export default function ConcertsPage() {
   const [feedItems, setFeedItems] = useState([]);
   useEffect(() => {
     getNewsFeed().then((feed) => setFeedItems(feed));
   }, []);
+
   return (
     <div className="w-screen">
       <div className="text-center font-inconsolata mt-10 text-[22px]">
@@ -18,11 +19,7 @@ export default function ConcertsPage() {
       <Separator className="my-4" />
       <div className="w-full flex flex-col items-center">
         {feedItems.map((feedItem) => (
-          <>
-            <Ticket key={feedItem.id} concertObj={feedItem.concert} />
-            <p>{feedItem.username}</p>
-            <p>{feedItem.created_at}</p>
-          </>
+          <FeedItem key={feedItem.id} feedItem={feedItem} />
         ))}
       </div>
     </div>
---
+++ src/components/feedItem.js
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { parseISO, format } from 'date-fns';
+import Ticket from './Ticket';
+
+export default function FeedItem({ feedItem }) {
+  const createdAt = format(parseISO(feedItem.created_at), 'MMM d, yyyy, h:mm a');
+
+  return (
+    <div className="mb-1 shadow p-4 ">
+      <p className="font-semibold">{feedItem.username}</p>
+      <Ticket concertObj={feedItem.concert} />
+      <p className="text-sm text-gray-400">Added {createdAt}</p>
+    </div>
+  );
+}
+
+FeedItem.propTypes = {
+  feedItem: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
+    concert: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      artist: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        spotify_id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      venue: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired,
+        setlist_fm_id: PropTypes.string.isRequired,
+      }).isRequired,
+      tour_name: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      time: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
