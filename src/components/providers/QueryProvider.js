d1bfa8242ac1b73b08d2afdc3716034f37508bc5
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-09-12 20:15:04 -0500

    chore: added QueryProvider to allow react query to function properly

---
+++ src/components/providers/QueryProvider.js
@@ -0,0 +1,31 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function QueryProvider({ children }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1 * 60 * 1000, // 5 minutes
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
+}
+
+QueryProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
